579a370277736e56f5db11858198e5b4ffa456ee
thierry <thierry.vdp@gmail.com>
2015-11-12 21:10:31 +0100

    gitignore

---
+++ Lucie/src/lucie/interfaces/INeuronController.java
@@ -0,0 +1,18 @@
+package lucie.interfaces;
+
+/**
+ * This interface describe what method are needed to handle a neuron interactions between user-data-display
+ * @author thierry
+ *
+ */
+public interface INeuronController {
+
+	// Getters
+	
+	
+	// Setters : usualy used when buiding a neuron or restoring from a backup
+
+
+	// Other methods
+	
+}
---
+++ Lucie/src/lucie/interfaces/INeuronGui.java
@@ -0,0 +1,18 @@
+package lucie.interfaces;
+
+/**
+ * This interface describe what method are needed to display a neuron on a graphic interface
+ * @author thierry
+ *
+ */
+public interface INeuronGui {
+
+	// Getters
+	
+	
+	// Setters : usualy used when buiding a neuron or restoring from a backup
+
+
+	// Other methods
+	
+}
