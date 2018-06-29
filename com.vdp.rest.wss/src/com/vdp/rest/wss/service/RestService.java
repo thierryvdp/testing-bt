56c5c30e2aa5c75e9021f7adf89e218c22c9f57a
batchoperator <batchoperator@vdplinux>
2018-06-29 17:29:33 +0200

    mongodb
    Signed-off-by: batchoperator <batchoperator@vdplinux>

--- com.vdp.rest.wss/src/com/vdp/rest/wss/service/RestService.java
+++ com.vdp.rest.wss/src/com/vdp/rest/wss/service/RestService.java
@@ -3,10 +3,6 @@ package com.vdp.rest.wss.service;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -18,19 +14,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoDatabase;
+
 @Path("/")
 public class RestService {
 
-	private Connection mysqlConnection;
+	//	private Connection mysqlConnection;
 
 	@PostConstruct
 	public void postConstruct() {
-		mysqlConnection = createConnection();
+		createConnection();
 	}
 
 	@PreDestroy
 	public void preDestroy() {
-		closeConnection(mysqlConnection);
+		//		closeConnection(mysqlConnection);
 	}
 
 	@POST
@@ -52,7 +53,7 @@ public class RestService {
 
 		// return HTTP response 200 in case of success
 		//		return Response.status(200).entity(crunchifyBuilder.toString()).build();
-		String result = "Json saved" + runQuery("select * from LU_Parameter where ParameterId like '%pdf%'");
+		String result = "Json saved";
 		return Response.status(201).entity(result).build();
 	}
 
@@ -67,58 +68,77 @@ public class RestService {
 		return Response.status(200).entity(result).build();
 	}
 
-	private Connection createConnection() {
+	private void createConnection() {
+		//		try {
+		//			Class.forName("com.mysql.jdbc.Driver");
+		//			return DriverManager.getConnection("jdbc:mysql://192.168.2.17/COMPUFIRST_2?user=root&password=Tdfxkp89;)");
+		//		} catch (Exception e) {
+		//			e.printStackTrace();
+		//		}
+		//		return null;
+
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection("jdbc:mysql://192.168.2.17/COMPUFIRST_2?user=root&password=Tdfxkp89;)");
+
+			String dbURI = "mongodb://thierry:rgjlvdp@192.168.2.69:27017";
+			MongoClient mongo = new MongoClient(new MongoClientURI(dbURI));
+
+			//			MongoClient mongo = new MongoClient("192.168.2.69", 27017);
+
+			// Creating Credentials 
+			MongoCredential credential;
+			credential = MongoCredential.createCredential("thierry", "dico", "rgjlvdp".toCharArray());
+			// Accessing the database 
+			MongoDatabase database = mongo.getDatabase("dico");
+			System.out.println("Credentials ::" + credential);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return null;
+
 	}
 
-	private String runQuery(String _query) {
-		Statement statement = null;
-		ResultSet resultSet = null;
-		String result = "";
-		try {
-			// Statements allow to issue SQL queries to the database
-			statement = mysqlConnection.createStatement();
-			// Result set get the result of the SQL query
-			resultSet = statement.executeQuery(_query);
-			while (resultSet.next()) {
-				result += resultSet.getString("ParameterId") + ";";
-				result += resultSet.getString("ParameterValue") + "\n";
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			closeResultSet(statement, resultSet);
-		}
-		return result;
-	}
-
-	private void closeConnection(Connection connect) {
-		try {
-			if (connect != null) {
-				connect.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void closeResultSet(Statement statement, ResultSet resultSet) {
-		try {
-			if (resultSet != null) {
-				resultSet.close();
-			}
-			if (statement != null) {
-				statement.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	//	private String runQuery(String _query) {
+	//		Statement statement = null;
+	//		ResultSet resultSet = null;
+	//		String result = "";
+	//		try {
+	//			// Statements allow to issue SQL queries to the database
+	//			statement = mysqlConnection.createStatement();
+	//			// Result set get the result of the SQL query
+	//			resultSet = statement.executeQuery(_query);
+	//			while (resultSet.next()) {
+	//				result += resultSet.getString("ParameterId") + ";";
+	//				result += resultSet.getString("ParameterValue") + "\n";
+	//			}
+	//		} catch (Exception e) {
+	//			e.printStackTrace();
+	//		} finally {
+	//			closeResultSet(statement, resultSet);
+	//		}
+	//		return result;
+	//	}
+	//
+	//	private void closeConnection(Connection connect) {
+	//		try {
+	//			if (connect != null) {
+	//				connect.close();
+	//			}
+	//		} catch (Exception e) {
+	//			e.printStackTrace();
+	//		}
+	//	}
+	//
+	//	private void closeResultSet(Statement statement, ResultSet resultSet) {
+	//		try {
+	//			if (resultSet != null) {
+	//				resultSet.close();
+	//			}
+	//			if (statement != null) {
+	//				statement.close();
+	//			}
+	//		} catch (Exception e) {
+	//			e.printStackTrace();
+	//		}
+	//	}
 
 }
