6d9ccdba74c5373f8e6ef84665c57710d9eee476
batchoperator <batchoperator@vdplinux>
2018-07-09 09:38:20 +0200

    jsondoc
    Signed-off-by: batchoperator <batchoperator@vdplinux>

--- com.vdp.rest.client/src/com/vdp/rest/wss/api/Document.java
+++ com.vdp.rest.client/src/com/vdp/rest/wss/api/Document.java
@@ -1,7 +1,11 @@
 package com.vdp.rest.wss.api;
 
+import java.util.List;
+
 public class Document {
 
-	private String documentId;
+	private String				documentId;
+	private List<FieldValue>	fields;
+	private List<Document>		documents;
 
 }
