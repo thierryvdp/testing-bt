81022f1c6e8aa173a51c0f88effa6a97688b1e34
batchoperator <batchoperator@vdplinux>
2018-08-03 16:57:38 +0200

    service image
    Signed-off-by: batchoperator <batchoperator@vdplinux>

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/PlaygroundPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/PlaygroundPart.java
@@ -2,7 +2,15 @@ package com.example.e4.rcp.todo.parts;
 
 import javax.annotation.PostConstruct;
 
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
+import org.eclipse.jface.resource.ResourceManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+import com.example.e4.bundleresourceloader.IBundleResourceLoader;
 
 public class PlaygroundPart {
 
@@ -11,8 +19,12 @@ public class PlaygroundPart {
 	}
 
 	@PostConstruct
-	public void createControls(Composite parent) {
+	public void createControls(Composite parent, IBundleResourceLoader loader) {
 		System.out.println(this.getClass().getSimpleName() + " @PostConstruct method called.");
+		Label label = new Label(parent, SWT.NONE);
+		ResourceManager resourceManager = new LocalResourceManager(JFaceResources.getResources(), label);
+		Image image = resourceManager.createImage(loader.getImageDescriptor(this.getClass(), "images/BIORIM.png"));
+		label.setImage(image);
 	}
 
 }
