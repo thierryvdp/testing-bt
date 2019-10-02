418222d4d377d35f46598d4ce07139b1a74064aa
thierryvdp <thierry.vdp@gmail.com>
2019-10-02 09:31:54 +0200

    Add files via upload
    test de l'api de l'insee pour recupérer les informations d'une société suivant son siret

---
+++ insee.api/src/insee/api/launcher/Launcher.java
@@ -0,0 +1,57 @@
+package insee.api.launcher;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.log4j.Logger;
+
+public class Launcher {
+
+	private static final Logger logger = Logger.getLogger(Launcher.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/** opensiret */
+		String token = null;
+		String response = null;
+		try {
+			// hack certif
+			SshUtil.createTrustedManager();
+			// http client
+			// HttpClient
+			CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(new SSLConnectionSocketFactory(SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build())).build();
+
+			// HttpPost Request
+			HttpPost postRequest = new HttpPost("https://api.insee.fr/token");
+			String encodedBytes = Base64.getEncoder().encodeToString(("6_ttyuk3QfleC7fj64IUF05hVUMa" + ":" + "IUqxBiG3mLhsG4wrd1ewCwYeFRka").getBytes());
+			postRequest.addHeader("Authorization", "Basic " + encodedBytes);
+			postRequest.setEntity(new StringEntity("grant_type=client_credentials", StandardCharsets.UTF_8));
+			HttpResponse httpPostResponse = httpClient.execute(postRequest);
+			token = FileUtil.dump(httpPostResponse.getEntity().getContent());
+
+			// HttpGet Request
+			HttpGet getRequest = new HttpGet("https://api.insee.fr/entreprises/sirene/V3/siret/85318202000018");
+			getRequest.addHeader("Accept", "application/json");
+			getRequest.addHeader("Authorization", "Bearer " + "94584011-403f-3a2d-b5a4-c1221f614664");
+			HttpResponse httpgetResponse = httpClient.execute(getRequest);
+			response = FileUtil.dump(httpgetResponse.getEntity().getContent());
+
+		} catch (Exception e) {
+			logger.error("", e);
+		}
+
+		logger.info("\ntoken:" + token + "\nresponse:" + response);
+
+	}
+
+}
---
+++ insee.api/src/insee/api/launcher/SshUtil.java
@@ -0,0 +1,38 @@
+package insee.api.launcher;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+@SuppressWarnings("unqualified-field-access")
+public class SshUtil {
+
+	public static void createTrustedManager() {
+		// Create a trust manager that does not validate certificate chains
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			@Override
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			@Override
+			public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+			}
+
+			@Override
+			public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+			}
+		} };
+
+		// Install the all-trusting trust manager
+		try {
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (Exception e) {
+		}
+
+	}
+
+}
