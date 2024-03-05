d3317718f70635d74bffb2fe4b69533f74fbef4d
thierryvdp <thierry.vdp@gmail.com>
2024-03-05 18:52:58 +0100

    msg

--- python/claude/claude.py
+++ python/claude/claude.py
@@ -1,13 +1,18 @@
+# pip install anthropic
+# https://console.anthropic.com/settings/plans
 import anthropic
 import os
 
+apiKey = os.environ['CLAUDE_API_KEY']
+print(apiKey)
+
 client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
-    api_key=os.environ['CLAUDE_API_KEY'],
+    api_key=apiKey
 )
 
 message = client.messages.create(
-    model="claude-3-opus-20240229,
+    model="claude-3-opus-20240229",
     max_tokens=1024,
     messages=[
         {"role": "user", "content": "Hello, Claude"}
