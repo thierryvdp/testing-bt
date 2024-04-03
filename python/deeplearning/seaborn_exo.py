c7d03a22be60b3d53007a1c756171f2459444706
thierryvdp <thierry.vdp@gmail.com>
2024-04-03 16:36:12 +0200

    windows

--- python/deeplearning/seaborn_exo.py
+++ python/deeplearning/seaborn_exo.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import csv
+
+# https://github.com/mwaskom/seaborn-data pour recup iris.csv
+
+iris = pd.read_csv('deeplearning/datasets/iris.csv')
+print(iris.head())
