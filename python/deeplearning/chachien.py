b320c80867e4f9053bbe89088900208778c36adc
thierryvdp <thierry.vdp@gmail.com>
2024-04-24 10:56:50 +0200

    windows

--- python/deeplearning/chachien.py
+++ python/deeplearning/chachien.py
@@ -8,11 +8,15 @@ def spec(dataset):
     print('Shape:',dataset.shape)
 
 def norm_aplati(dataset):
-    # 1. normaliser les datas 0-255 -> 0-1
-    dataset = dataset/255
-    # 2. aplatir -> dataset.shape[0]x64x64 -> 4096
-    dataset = dataset.reshape(dataset.shape[0], 4096)
-    return dataset
+    # 1. aplatir -> dataset.shape[0]x64x64 -> 4096
+    # 2. normaliser les datas 0-255 -> 0-1
+    # formule normale : (X - Xmin)/(Xmax - Xmin)
+    # un peu facile ...  Xmin=0 Xmax=255 donc  dataset_wrk = dataset/255
+    # return dataset.reshape(dataset_wrk.shape[0], 4096).max()
+    # return dataset.reshape(dataset_wrk.shape[0], dataset_wrk.shape[1] * dataset_wrk.shape[2]).max()
+    # return dataset.reshape(dataset.shape[0], -1) / dataset.max()
+    # max marche pas car il faut garder le max du train set pour normaliser de la même façon
+    return dataset.reshape(dataset.shape[0], -1) / 254
 
 
 print('====================================================')
@@ -26,18 +30,26 @@ spec(y_test)
 
 # 1. normaliser les datas 0-255 -> 0-1
 # 2. aplatir -> 64x64 -> 4096
-X_train = norm_aplati(X_train)
-X_test = norm_aplati(X_test)
+X_train_reshape = norm_aplati(X_train)
+X_test_reshape = norm_aplati(X_test)
 
 # 3. entrainement (peut être utiliser sklearn au lieu de notre log_loss)
-poids, biais, log_loss = ut.artificial_neuron(X_train, y_train)
+poids, biais, log_loss, accuracy_list, test_loss, test_accuracy = ut.artificial_neuron(X_train_reshape, y_train, X_test_reshape, y_test, learning_rate=0.01, n_iter=10000)
 
 # 4. tracer la courbe d'apprentissage ; nb si notre log_loss marche mal
 # utiliser celle de sklearn du module metrics
-plt.plot(log_loss)
+plt.figure(figsize=(12,4))
+plt.subplot(1,2,1)
+plt.plot(log_loss, label='train loss')
+plt.plot(test_loss, label='test loss')
+plt.legend()
+plt.subplot(1,2,2)
+plt.plot(accuracy_list, label='train accuracy')
+plt.plot(test_accuracy, label='test  accuracy')
+plt.legend()
 plt.show()
 
 # 5. evaluer le modele sur le test
-print(ut.predict(X_test[0], poids, biais))
+print(ut.predict(X_test_reshape[0], poids, biais))
 
 
--- python/deeplearning/utilities.py
+++ python/deeplearning/utilities.py
@@ -3,6 +3,7 @@ import h5py
 import numpy as np
 from sklearn.metrics import log_loss, accuracy_score
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 def load_data():
     train_dataset = h5py.File('deeplearning/datasets/trainset.hdf5', "r")
@@ -22,11 +23,15 @@ def init_poids_biais(x_dataset):
 
 def model(x_dataset, poids, biais):
     sortie = x_dataset.dot(poids) + biais
+    # l'exponentielle fait des overflow ...
+    # cela rempli la matrice d'activation avec des 1 et des 0
     proba_activation = 1 / (1 + np.exp(-sortie))
     return proba_activation
 
 def log_loss(activation, y_dataset):
-   return 1 / len(y_dataset) * np.sum( -y_dataset * np.log(activation) - (1 - y_dataset) * np.log(1 - activation))
+    # on ajoute un epsilon pour eviter les 1 et 0 de la matrice d'activation
+   epsilon = 1e-15
+   return 1 / len(y_dataset) * np.sum( -y_dataset * np.log(activation + epsilon) - (1 - y_dataset) * np.log(1 - activation  + epsilon))
 
 def gradients(activation, x_dataset, y_dataset):
     dW = 1 / len(y_dataset) * np.dot(x_dataset.T, activation - y_dataset)
@@ -38,21 +43,37 @@ def update(delta_poids, delta_biais, poids, biais, learning_rate):
     biais = biais - learning_rate * delta_biais
     return (poids, biais)
 
-def artificial_neuron(x_dataset, y_dataset, learning_rate = 0.1, n_iter = 100):
+def artificial_neuron(x_dataset, y_dataset, x_test_dataset, y_test_datatest, learning_rate = 0.1, n_iter = 100):
     #initialiser W, b
     poids, biais = init_poids_biais(x_dataset)
     # etude du cout qui doit diminuer ...
     loss_list = []
-    for i in range(n_iter):
+    accuracy_list = []
+    # etude de test
+    test_loss = []
+    test_accuracy = []
+    
+    for i in tqdm(range(n_iter)):
+        # activation
         activation= model(x_dataset, poids, biais)
-        loss_list.append(log_loss(activation, y_dataset))
+        if i %10 == 0:
+            # calcul du cout du trainset
+            loss_list.append(log_loss(activation, y_dataset))
+            # calcul de l'accuracy du trainset
+            y_pred = predict(x_dataset, poids, biais)
+            accuracy_list.append(accuracy_score(y_dataset, y_pred))
+            
+            # calcul du cout du testset
+            activationTest= model(x_test_dataset, poids, biais)
+            test_loss.append(log_loss(activationTest, y_test_datatest))
+            # calcul de l'accuracy du testset
+            y_pred = predict(x_test_dataset, poids, biais)
+            test_accuracy.append(accuracy_score(y_test_datatest, y_pred))
+            
+        # mise à jour
         dW, db = gradients(activation, x_dataset, y_dataset)
-        poids, biais = update(dW, db, poids, biais, learning_rate)   
-    # y_pred = predict(x_dataset, poids, biais)
-    # print(accuracy_score(y_dataset, y_pred))
-    # plt.plot(loss_list)
-    # plt.show()
-    return poids, biais, loss_list
+        poids, biais = update(dW, db, poids, biais, learning_rate)
+    return poids, biais, loss_list, accuracy_list, test_loss, test_accuracy
 
 def predict(x_dataset, poids, biais):
     proba_activation = model(x_dataset, poids, biais)
